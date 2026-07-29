0b211bb8ca6031334da1f08010a377c91be75cf6
Goodman Gift <eseebikeme@gmail.com>
2026-07-29 18:34:32 +0100

    Remove app_name declaration from urls.py
    Removed app_name declaration from urls.py.

--- managers/urls.py
+++ managers/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'managers'
-
 urlpatterns = [
     path('managers/dashboard/', views.manager_dashboard, name='manager_dashboard'),
     path('valets/manage/', views.manage_valets, name='manage_valets'),
